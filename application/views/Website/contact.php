c6fbcdaeb1fa94f368c47c0afeef2590f2a6649b
technothinksup <project.technothinksup@gmail.com>
2020-02-20 17:18:54 +0530

    final

--- application/views/Website/contact.php
+++ application/views/Website/contact.php
@@ -20,15 +20,15 @@
                 <div id="errormessage"></div>
                   <form action="<?php echo base_url();?>Website/send_mail" method="post"  role="form" class="contactForm">
                     <div class="form-group">
-                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                      <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars"  required/>
                       <div class="validation"></div>
                     </div>
                     <div class="form-group">
-                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                      <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" required />
                       <div class="validation"></div>
                     </div>
                     <div class="form-group">
-                      <input type="text" class="form-control" name="mobile" id="nobile" placeholder="Your Mobile No." data-rule="minlen:10" data-msg="Please enter a valid Mobile No." />
+                      <input type="text" class="form-control" name="mobile" id="nobile" placeholder="Your Mobile No." data-rule="minlen:10" data-msg="Please enter a valid Mobile No."  required/>
                       <div class="validation"></div>
                     </div>
                     <div class="form-group">
